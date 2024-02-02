ef9544c955a95ba66ec43a8520770b1e11e65157
Phantom-AL <den2005den0715@gmail.com>
2024-02-02 17:38:16 +0200

    slug converter for product

--- goods/views.py
+++ goods/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+
 from goods.models import Products
 
 
@@ -12,5 +13,10 @@ def catalog(request):
     return render(request, 'goods/catalog.html', context)
 
 
-def product(request):
-    return render(request, 'goods/product.html')
+def product(request, product_slug):
+
+    product = Products.objects.get(slug=product_slug)
+    context = {
+        'product': product
+    }
+    return render(request, 'goods/product.html', context)
